49e1b9ad0d39582de8ed363f186f6b0b5793890d
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-03-20 15:20:04 +0500

    Entity

---
+++ CLOUPARD-Test task MVC/CLOUPARD-Test task MVC/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace CLOUPARD_Test_task_MVC.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
--- CLOUPARD-Test task/CLOUPARD-Test task/Entities/Product.cs
+++ CLOUPARD-Test task/CLOUPARD-Test task/Entities/Product.cs
@@ -1,8 +1,12 @@
-﻿namespace CLOUPARD_Test_task.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CLOUPARD_Test_task.Entities
 {
     public class Product
     {
+        [Key]
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
     }
